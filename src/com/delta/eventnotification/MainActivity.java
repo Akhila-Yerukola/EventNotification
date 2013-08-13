04937496f8f4ba7ea734b1e8e117298eb7ec2e5b
Akhila-Yerukola <yakhila_04@yahoo.co.in>
2013-08-14 01:44:59 +0530

    Database and tab host added

--- src/com/delta/eventnotification/MainActivity.java
+++ src/com/delta/eventnotification/MainActivity.java
@@ -1,8 +1,12 @@
 package com.delta.eventnotification;
 
+
+
 import android.os.Bundle;
 import android.app.Activity;
 import android.view.Menu;
+import android.widget.TabHost;
+import android.widget.TabHost.TabSpec;
 
 public class MainActivity extends Activity {
 
@@ -10,13 +14,18 @@ public class MainActivity extends Activity {
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
+		TabHost th = (TabHost) findViewById(R.id.tabhost);
+		th.setup();
+		TabSpec specs = th.newTabSpec("tag1");
+		specs.setContent(R.id.tab1);
+		specs.setIndicator("Events");
+		th.addTab(specs);
+		specs = th.newTabSpec("tag2");
+		specs.setContent(R.id.tab2);
+		specs.setIndicator("Notifications");
+		th.addTab(specs);
 	}
 
-	@Override
-	public boolean onCreateOptionsMenu(Menu menu) {
-		// Inflate the menu; this adds items to the action bar if it is present.
-		getMenuInflater().inflate(R.menu.main, menu);
-		return true;
-	}
+	
 
 }
