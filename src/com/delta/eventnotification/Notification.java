ffda7a6c52a0d339ab69141b6e3419df065e93e7
Akhila-Yerukola <yakhila_04@yahoo.co.in>
2013-08-14 00:42:14 +0530

    Blank project

---
+++ src/com/delta/eventnotification/Notification.java
@@ -0,0 +1,22 @@
+package com.delta.eventnotification;
+
+import android.os.Bundle;
+import android.app.Activity;
+import android.view.Menu;
+
+public class Notification extends Activity {
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.notification);
+	}
+
+	@Override
+	public boolean onCreateOptionsMenu(Menu menu) {
+		// Inflate the menu; this adds items to the action bar if it is present.
+		getMenuInflater().inflate(R.menu.notification, menu);
+		return true;
+	}
+
+}
